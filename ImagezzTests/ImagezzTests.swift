d7d4c3dc40fbed06f7f0933dcb70303050ad0e98
Frane Poljak <frane.poljak@strainprint.ca>
2021-01-25 17:52:11 +0100

    Initial Commit

---
+++ ImagezzTests/ImagezzTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImagezzTests.swift
+//  ImagezzTests
+//
+//  Created by Frane Poljak on 25/01/2021.
+//  Copyright © 2021 fpoljak. All rights reserved.
+//
+
+import XCTest
+@testable import Imagezz
+
+class ImagezzTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
